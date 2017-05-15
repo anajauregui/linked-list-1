36bb41d55a36a0b37c644391808ef466915e2887
Ana <analujauregui@gmail.com>
2017-05-15 10:42:38 -0600

    Started var list

---
+++ scripts.js
@@ -0,0 +1,6 @@
+var webTitle = document.getElementById('web-title');
+var webUrl = document.getElementById('web-url');
+var enter = document.getElementById('enter');
+var webCard = document.querySelectorAll('.web-cards');
+var readCard = document.getElementById('read-link');
+var deleteCard = document.getElementById('delete-link');
