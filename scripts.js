f62230602506241c31490b6941975adb01842feb
Ana <analujauregui@gmail.com>
2017-05-18 08:17:49 -0600

    Final completed project

--- scripts.js
+++ scripts.js
@@ -11,7 +11,6 @@ var webTitleValue = webTitle.value;
 var webUrlValue = webUrl.value;
 
 
-////CODE RE-WORKED, ENTER BUTTON NOW FUNCTIONAL TO BE DISABLED.
 function enableButton() {
   var title = $('#web-title').val();
   var url = $('#web-url').val();
@@ -24,7 +23,6 @@ function enableButton() {
 }
 $('#web-title, #web-url').on('input', enableButton);
 
-/////// Validating URL /////
 function validUrl() {
     var userInput = $('#web-url').val()
     var regexQuery = "^(https?://)?(www\\.)?([-a-z0-9]{1,63}\\.)*?[a-z0-9][-a-z0-9]{0,61}[a-z0-9]\\.[a-z]{2,6}(/[-\\w@\\+\\.~#\\?&/=%]*)?$";
@@ -32,14 +30,12 @@ function validUrl() {
 
     if (url.test(userInput)) {
       createBookmark();
-      // linksCounter();
     } else {
       alert(userInput + ' is an invalid URL');
     return false;
   }
 }
 
-////<<<<THIS IS NECESSARY TO PROPERLY RUN OUR CODE- UNTIL WE REWORK THE ENTER DISABLE BUTTON>>>>>>>>>>>>>>>
 function inputComplete() {
   if (webTitle.value === "" || webUrl.value === "") {
     alert("Please complete all input fields")
@@ -47,7 +43,7 @@ function inputComplete() {
     validUrl();
   }
 }
-//// Added lines 37, 38 to clear fields upon bookmark creation.
+
 enter.addEventListener('click', function(){
   inputComplete();
   webTitle.value = '';
@@ -65,14 +61,12 @@ function createBookmark() {
   webCard.innerHTML = `<article class="web-cards">
     <h3 class="card-web-title">  ${webTitle.value}  </h3>
     <hr id="hr1">
-    <a id="card-web-link" class="links" href="${"http://" + webUrl.value}">  ${webUrl.value}  </a>
+    <a id="card-web-link" class="links text-decoration" href="${"http://" + webUrl.value}">  ${webUrl.value}  </a>
     <hr id="hr2">
     <button id="read-link" class="read-delete read-button" type="button">Read</button>
-    <button id="delete-link" class="read-delete delete-button" type="button">Delete</button>
+    <button id="delete-link" class="read-delete delete-button text-decoration" type="button">Delete</button>
   </article>`
 }
-
-////When .read has been added document.createElement('button') at the top of the bookmarks section to clear all read bookmarks. this should be accomplished with .remove() of all cards with the class of .read
 
 $('.card-stack')
   .on('click', 'button.read-button', toggleReadCards)
@@ -86,6 +80,8 @@ function toggleReadCards() {
   $(this).toggleClass('read');
   $(this).parent().toggleClass('backgroundColor');
   $('#clear-read').prop('disabled', false);
+  $(this).siblings('#card-web-link').toggleClass('text-decoration');
+  $(this).siblings('#delete-link').toggleClass('text-decoration');
   readBookmarks();
 }
 
@@ -95,7 +91,7 @@ function removeCards() {
   readBookmarks();
 }
 
-function bookmarkCounter()  {
+function bookmarkCounter() {
    var $bookmarkCount = $('.web-cards').length;
    $('.total-bookmark-counter').text($bookmarkCount);
  }
